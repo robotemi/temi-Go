4d848963c24d62389993d4d7a0b80f65d9684825
shawnrobocore <144330455+shawnrobocore@users.noreply.github.com>
2024-12-06 16:42:08 +0800

    remove unused import

--- feature-delivery/src/main/java/com/robotemi/go/feature/delivery/di/ResourcesProvider.kt
+++ feature-delivery/src/main/java/com/robotemi/go/feature/delivery/di/ResourcesProvider.kt
@@ -1,12 +1,7 @@
 package com.robotemi.go.feature.delivery.di
 
 import android.content.Context
-import android.net.Uri
-import android.os.Environment
-import androidx.annotation.StringRes
-import androidx.core.net.toUri
 import dagger.hilt.android.qualifiers.ApplicationContext
-import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
 
